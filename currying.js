4da765cc04666cc5f5aeb292313f4fe6ee61612d
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2024-04-14 14:21:23 +0530

    Create currying.js

---
+++ currying.js
@@ -0,0 +1,23 @@
+// Currying question
+
+function finalString(...args){
+  
+  const parts = args.filter(value => value!==undefined && value!==null)
+  
+   function collector(...next){
+    if(next!== undefined && next!==null){
+      parts.push(...next.flat())
+     }
+    return collector
+  }
+  collector.toString = function(){
+    return parts.join("_")
+  }
+  
+  return collector
+  
+}
+
+let result = finalString(2)(3)(5)(7)(8)()()(4)([5,8])(5)(6,7,8)
+
+console.log(result.toString()) // Output: "2_3_5_7_8_4_5_8_5"
