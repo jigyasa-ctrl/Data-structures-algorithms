4f9feec0b11c2adcd399eb89f415f35ab05fbde3
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2024-05-03 13:43:06 +0530

    Update promise-all.js

--- promise-all.js
+++ promise-all.js
@@ -1,5 +1,10 @@
 // promise.all() polyfill
 
+// The Promise.all() accepts an array of promises and returns a promise
+// that resolves when all of the promises in the array are fulfilled or when
+// the iterable contains no promises. It rejects with the reason of the first
+// promise that rejects.
+
 const myPromiseAll = function (taskList) {
   let result = []
   let promiseCompleted = 0
