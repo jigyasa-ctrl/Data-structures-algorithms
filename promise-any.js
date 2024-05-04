fbbabb6b7884a9dc4da3e3b52bbf9282a1bff183
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2024-05-04 13:19:23 +0530

    Update promise-any.js

--- promise-any.js
+++ promise-any.js
@@ -1,5 +1,12 @@
 // promise.any Polyfill
 
+// Promise.any() takes an iterable of Promise objects. It returns a single
+// promise that fulfills as soon as any of the promises in the iterable
+// fulfills, with the value of the fulfilled promise. If no promises in the
+// iterable fulfill (if all of the given promises are rejected), then the
+// returned promise is rejected with an AggregateError, a new subclass of
+// Error that groups together individual errors.
+
     const any = function(tasks){
 
     // set length for errors
