b68a0deca919ab3246719e0fb3d3801abee0bc0b
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2024-09-22 18:07:13 +0530

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,6 +1,10 @@
 // Javascript does not have any inbuilt function for this, But thanks to
 // the introduction of promises and async-await in ES2018
 
+// this does not
+// stop the entire program execution like it does in other programming
+// languages, instead it will just make our current function sleep.
+
 let sleep = function(delay) => {
   return new Promise(resolve => setTimeout(resolve, delay))
 }
