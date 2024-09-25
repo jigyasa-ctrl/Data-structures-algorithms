8e114f7fd7c94f8438cd59e054e9cbca76f013b4
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2024-09-25 22:55:08 +0530

    Create recurrsion.js

---
+++ recurrsion.js
@@ -0,0 +1,26 @@
+
+const sumTo =(n) => {
+  let sum = 0
+  if(n == 1){
+    return 1
+  } else {
+    return n + sumTo(n-1)
+  }
+}
+
+const factorial =(n) => {
+  let sum = 0
+  if(n == 1){
+    return 1
+  } else {
+    return n * sumTo(n-1)
+  }
+}
+
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+console.log(sumTo(3))
+console.log(factorial(3))
+console.log(fib(3))
