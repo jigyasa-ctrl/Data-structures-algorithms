db0a5e5146e9c25cf89020d4d1d6ef786fdd56ab
Jigyasa Upadhyay <jigyasaupadhyay60@gmail.com>
2026-05-05 20:42:28 +0530

    leading trailing added

--- throttle-with-arguments.js
+++ throttle-with-arguments.js
@@ -19,3 +19,38 @@ function throttle(func, wait) {
 
 
   // this is to be used when you want the arguments of latest call to be used in the next throtttle call
+
+
+
+
+  // throttle with leading and trailing
+
+  function throttle(func, delay, options) {
+    let { leading, trailing } = options
+    let timer
+    let isRunning = false
+    let lastArgs
+    let lastContext
+    return function (...args) {
+      lastContext = this
+  
+      if (!isRunning) {
+        if (leading) {
+          func.call(this, ...args)
+          clearTimeout(timer)
+        }
+  
+        isRunning = true
+  
+        timer = setTimeout(() => {
+          isRunning = false
+          if (lastArgs && trailing) {
+            func.apply(this, lastArgs)
+            lastArgs = null
+          }
+        }, delay)
+      } else if (trailing) {
+        lastArgs = [...args]
+      }
+    }
+  }
